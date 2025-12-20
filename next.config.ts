65eaa345c56776d021bd9bd1b380fb5f469a42b5
adreaskar <adrian26mccous@gmail.com>
2025-12-20 10:41:46 +0200

    security updates

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
     serverActions: {
-      allowedOrigins: ["dashboard.ai4gov-project.eu", "localhost:3000"],
+      allowedOrigins: ["localhost:3000"],
     },
   },
   reactStrictMode: false,
